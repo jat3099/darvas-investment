6a5212be44d587b7309dd346a6b7cf1de2b762b0
jat3099 <jat3099@hotmail.com>
2025-08-02 20:03:48 -0400

    Changes to index.tsx

--- index.tsx
+++ index.tsx
@@ -2,14 +2,16 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+// 1. Import HashRouter instead of BrowserRouter
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <BrowserRouter basename="/darvas-investment/">
+        {/* 2. Use HashRouter here */}
+        <HashRouter>
             <App />
-        </BrowserRouter>
+        </HashRouter>
     </React.StrictMode>,
 );
